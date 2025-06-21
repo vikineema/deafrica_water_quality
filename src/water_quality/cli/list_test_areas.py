e496e5f3ff225145d7166c6ffa30ca7be73a70ec
Victoria Neema <victoria@kartoza.com>
2025-06-22 00:59:24 +0300

    Add cli for getting tasks to run for

---
+++ src/water_quality/cli/list_test_areas.py
@@ -0,0 +1,19 @@
+from importlib.resources import files
+
+import click
+import geopandas as gpd
+import pandas as pd
+
+
+@click.command(
+    name="list-test-areas",
+)
+def cli():
+    """
+    View the names of defined test areas available to run the DE Africa Water Quality workflow on.
+    """
+    places_fp = files("water_quality.data").joinpath("places.parquet")
+    places_gdf = gpd.read_parquet(places_fp)
+    columns = ["name", "desc"]
+    with pd.option_context("display.max_rows", None):
+        print(places_gdf[columns])
