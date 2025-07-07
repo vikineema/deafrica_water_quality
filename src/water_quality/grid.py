7651f2d4fc2f16f259f5a776f5f446ce95a08e58
Victoria Neema <victoria@kartoza.com>
2025-07-07 10:38:11 +0300

    Parse resolution to gridspec for tiles

--- src/water_quality/grid.py
+++ src/water_quality/grid.py
@@ -2,18 +2,32 @@ from odc.geo import XY, Resolution
 from odc.geo.gridspec import GridSpec
 
 
-class WaterbodiesGrid:
-    def __init__(self):
-        self.gridname = "waterbodies_grid"
-        self.resolution = Resolution(y=-10, x=10)
-        # Match the tile size for DE Africa GeoMADs tiles
-        self.tile_size = XY(y=96000, x=96000)
-        self.gridspec = GridSpec(
-            crs="EPSG:6933",
-            tile_shape=XY(
-                y=self.tile_size.y / abs(self.resolution.y),
-                x=self.tile_size.x / abs(self.resolution.x),
-            ),
-            resolution=self.resolution,
-            origin=XY(y=-7392000, x=-17376000),
-        )
+def get_waterbodies_grid(resolution: int) -> GridSpec:
+    """
+    Get the grid to be used for tiling in the DE Africa
+    Water Quality continental workflow.
+
+    Parameters
+    ----------
+    resolution : int
+        X and Y resolution as an absolute integer.
+
+    Returns
+    -------
+    GridSpec
+        Gridspec to be used for tiling.
+    """
+    resolution = abs(resolution)
+    tile_size = 96000
+    tile_shape = tile_size / resolution
+
+    gridspec = GridSpec(
+        crs="EPSG:6933",
+        tile_shape=XY(
+            y=tile_shape,
+            x=tile_shape,
+        ),
+        resolution=Resolution(y=-resolution, x=resolution),
+        origin=XY(y=-7392000, x=-17376000),
+    )
+    return gridspec
