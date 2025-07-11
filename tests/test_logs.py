315c45839bde08e726c891b9650abda453dd70ca
Victoria Neema <victoria@kartoza.com>
2025-07-11 23:38:24 +0300

    Remove most tests until w.p 1.4 is complete

--- tests/test_logs.py
+++ tests/test_logs.py
@@ -7,9 +7,9 @@ from water_quality.logs import setup_logging
 
 def test_logging_level_behavior_level_1(caplog):
     logger = setup_logging(verbose=1)
-    with caplog.at_level(logging.DEBUG):
-        logger.critical("This is a Critical message")
-        logger.error("This is an Error message")
+
+    logger.critical("This is a Critical message")
+    logger.error("This is an Error message")
 
     record_levelnames = [record.levelname for record in caplog.records]
     assert "CRITICAL" in record_levelnames
@@ -18,10 +18,9 @@ def test_logging_level_behavior_level_1(caplog):
 
 def test_logging_level_behavior_level_2(caplog):
     logger = setup_logging(verbose=2)
-    with caplog.at_level(logging.DEBUG):
-        logger.critical("This is a Critical message")
-        logger.error("This is an Error message")
-        logger.warning("This is a Warning message")
+    logger.critical("This is a Critical message")
+    logger.error("This is an Error message")
+    logger.warning("This is a Warning message")
 
     record_levelnames = [record.levelname for record in caplog.records]
     assert "CRITICAL" in record_levelnames
@@ -31,11 +30,11 @@ def test_logging_level_behavior_level_2(caplog):
 
 def test_logging_level_behavior_level_3(caplog):
     logger = setup_logging(verbose=3)
-    with caplog.at_level(logging.DEBUG):
-        logger.critical("This is a Critical message")
-        logger.error("This is an Error message")
-        logger.warning("This is a Warning message")
-        logger.info("This is an Info message")
+   
+    logger.critical("This is a Critical message")
+    logger.error("This is an Error message")
+    logger.warning("This is a Warning message")
+    logger.info("This is an Info message")
 
     record_levelnames = [record.levelname for record in caplog.records]
     assert "CRITICAL" in record_levelnames
@@ -46,12 +45,11 @@ def test_logging_level_behavior_level_3(caplog):
 
 def test_logging_level_behavior_level_4(caplog):
     logger = setup_logging(verbose=4)
-    with caplog.at_level(logging.DEBUG):
-        logger.critical("This is a Critical message")
-        logger.error("This is an Error message")
-        logger.warning("This is a Warning message")
-        logger.info("This is an Info message")
-        logger.debug("This is a Debug message")
+    logger.critical("This is a Critical message")
+    logger.error("This is an Error message")
+    logger.warning("This is a Warning message")
+    logger.info("This is an Info message")
+    logger.debug("This is a Debug message")
 
     record_levelnames = [record.levelname for record in caplog.records]
     assert "CRITICAL" in record_levelnames
@@ -63,12 +61,11 @@ def test_logging_level_behavior_level_4(caplog):
 
 def test_logging_level_behavior_level_5(caplog):
     logger = setup_logging(verbose=5)
-    with caplog.at_level(logging.DEBUG):
-        logger.critical("This is a Critical message")
-        logger.error("This is an Error message")
-        logger.warning("This is a Warning message")
-        logger.info("This is an Info message")
-        logger.debug("This is a Debug message")
+    logger.critical("This is a Critical message")
+    logger.error("This is an Error message")
+    logger.warning("This is a Warning message")
+    logger.info("This is an Info message")
+    logger.debug("This is a Debug message")
 
     record_levelnames = [record.levelname for record in caplog.records]
     assert "CRITICAL" in record_levelnames
