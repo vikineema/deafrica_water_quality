7953cb69ad389428263374e656a375b9c4d27d82
Victoria Neema <victoria@kartoza.com>
2025-11-20 23:20:21 +0300

    Update create_OWT_response_models

--- src/water_quality/mapping/optical_water_type.py
+++ src/water_quality/mapping/optical_water_type.py
@@ -7,13 +7,14 @@ import xarray as xr
 
 log = logging.getLogger(__name__)
 
+
 def get_OWT_description(owt):
     """
     Get description of a OWT value
     """
-    if not (1<=owt<=13):
+    if not (1 <= owt <= 13):
         raise ValueError("OWT value must be between 1 and 13")
-        
+
     data = [
         [3, "oligotrophic (clear)"],
         [9, "oligotrophic (clear)"],
@@ -35,77 +36,93 @@ def get_OWT_description(owt):
     df = pd.DataFrame(data=data, columns=columns)
     desc = df[df["OWT"] == owt]["description"].item()
     return desc
-    
-def create_OWT_response_models() -> pd.DataFrame:
-    """
-    Create Optical Water Type (OWT) response models and save to csv files.
 
-    Parameters
-    ----------
-    agm : bool, optional
-        Whether to create models for geomedian instruments, by default True.
+
+def create_OWT_response_model() -> dict[str, pd.DataFrame]:
+    """
+    Create Optical Water Type (OWT) response models and write to
+    csv files for all supported instruments.
 
     Returns
     -------
-    pd.DataFrame
-        DataFrame containing OWT response models.
+    dict[str, pd.DataFrame]
+        Dictionary of DataFrames containing OWT response models for each
+        instrument
     """
-    
+    sensors = [
+        "msi",
+        "tm",
+        "oli",
+    ]
+
     OWT_spectra_fp = files("water_quality.data").joinpath(
         "Vagelis_OWT_allwaters_mean_standardised.csv"
     )
     OWT_spectra = pd.read_csv(OWT_spectra_fp)
     # Limit to inland water types
-    OWT_spectra = OWT_spectra[OWT_spectra.wl <= 13]
+    OWT_spectra = OWT_spectra[OWT_spectra.index < 13]
 
     data = {}
 
     # Read in the wavelengths for the bands in oli, tm and msi
     # (spectral response models)
-    for sensor in ["msi", "tm", "oli"]:
-        sensor_data_fp = files("water_quality.data").joinpath(
-            f"sensor bands-{sensor}.csv"
-        )
+    for sensor in sensors:
+        # (spectal response models)
+        if sensor.endswith("_agm"):
+            suffix = "_agm"
+            sensor_data_fp = files("water_quality.data").joinpath(
+                f"sensor bands-{sensor[: -len(suffix)]}.csv"
+            )
+        else:
+            suffix = ""
+            sensor_data_fp = files("water_quality.data").joinpath(
+                f"sensor bands-{sensor}.csv"
+            )
         sensor_data = pd.read_csv(sensor_data_fp)
+        sensor_data["band_name"] = sensor_data["band_name"] + suffix
 
-        a = sensor_data
-        # list the bands  that are going to be relevant , ie., less than 800nm
-        band_list = (
-            a[a["max"] < 800].band_name
-        ).values  # & set(ds.data_vars)
-
-        # Set up a  dataframe to take results for this instrument
-        labels = []
-        for i in np.arange(1, 14):
-            labels = np.append(labels, "OWT-" + str(i))
-
-        inst_OWT = pd.DataFrame(
-            data={"OWT": np.arange(1, 14, 1)}, index=labels
+        # Rename columns to avoid clashes with reserved words
+        sensor_data.rename(
+            columns={"max": "l_max", "min": "l_min"}, inplace=True
         )
-        # Run through the bands ...
-        for band_name in band_list:
-            # determine the integration interval based on the central
-            # wavlength and the width; extract these as integers
-            delta = a[a["band_name"] == band_name]["width"].values * 0.8 * 0.5
-            start = int(
-                (a[a["band_name"] == band_name]["central"].values - delta)[0]
-            )
-            end = int(
-                (a[a["band_name"] == band_name]["central"].values + delta)[0]
-            )
-            # print(sensor,band_name, start,end)
 
-            # find the start and end column numbers in the response functions
-            # and sum over those for each OWT
-            b = OWT_spectra.loc[:, str(start) : str(end)].T.sum()
-            # add to the data frame
+        # set up a  dataframe to take results for this instrument
+        owt_values = OWT_spectra["wl"].to_list()
+        owt_labels = [f"OWT-{i}" for i in owt_values]
+        inst_OWT = pd.DataFrame(data={"OWT": owt_values}, index=owt_labels)
+
+        # list the bands  that are going to be relevant , ie., less than 800nm
+        band_list = sensor_data[sensor_data["l_max"] < 800][
+            "band_name"
+        ].to_list()
+
+        for band_name in band_list:
+            band_data = sensor_data[
+                sensor_data["band_name"] == band_name
+            ].iloc[0]
+
+            # Determine the integration interval based on the central
+            # wavlength and the width
+            delta = band_data["width"] * 0.8 * 0.5
+            central = band_data["central"]
+
+            # Find start and end column numbers in the response functions
+            # and average over those for each OWT, must allow for the
+            # spectral band width.
+            start = str(int(central - delta))
+            end = str(int(central + delta))
             inst_OWT.insert(
-                inst_OWT.columns.size, band_name, (b.values)
+                inst_OWT.columns.size,
+                band_name,
+                OWT_spectra.loc[:, start:end].T.mean().to_list(),
             )
-        # --- add this data frame to the data dictionary ---
+            inst_OWT.to_csv(
+                files("water_quality.data").joinpath(
+                    f"{sensor}_OWT_vectors.csv"
+                )
+            )
+            log.info(f"Written OWT response model for {sensor} to csv file.")
         data[sensor] = inst_OWT
-        # --- write to a csv file in the current location ---
-        inst_OWT.to_csv(f'{sensor}_OWT_vectors.csv')
     return data
 
 
@@ -136,10 +153,10 @@ def OWT(
 
     dp_corrected: bool, optional
         Whether to use Rayleigh corrected bands as input. Default is False.
-        
+
     check_type: bool, optional
         Whether to check the prevailing water type in intermediate steps. Default is False.
-        
+
     Returns
     -------
     xr.DataArray
@@ -149,7 +166,8 @@ def OWT(
     # --- this version uses a long-hand approach to calculating the dot product. Inelegant but simple.
     # --- identify the instrument bands that are relevant and in the dataset
     log.info(
-        f"Calculating Optical Water Type (OWT) for instrument: {instrument}")
+        f"Calculating Optical Water Type (OWT) for instrument: {instrument}"
+    )
     suffix = ""
     if agm:
         suffix = suffix + "_agm"
@@ -181,7 +199,9 @@ def OWT(
         ds[varname], dtype=float, fill_value=-2
     )  # a number smaller than any cosine
     owt_closest = xr.full_like(
-        ds[varname], dtype="uint8", fill_value=int(OWT_vectors.index[0].split("-")[1])
+        ds[varname],
+        dtype="uint8",
+        fill_value=int(OWT_vectors.index[0].split("-")[1]),
     )
 
     for idx, vec in OWT_vectors.iterrows():
@@ -234,11 +254,12 @@ def run_OWT(ds: xr.Dataset) -> xr.Dataset:
     suffix = "_agm"
     for instrument in ["msi", "oli", "tm"]:
         inst_agm = instrument + suffix
-        
+
         # OWT_vector only depends on the instrument
         OWT_vectors = pd.read_csv(
             files("water_quality.data").joinpath(
-                f"{instrument}_OWT_vectors.csv"),
+                f"{instrument}_OWT_vectors.csv"
+            ),
             index_col=0,
         )
         # Use the count band as an indicator that data for the geomedian
@@ -253,7 +274,9 @@ def run_OWT(ds: xr.Dataset) -> xr.Dataset:
             )
             varname = instrument + "_agm_owt"
             if varname in ds.data_vars:
-                ds[varname] = xr.where(~np.isnan(OWT_data), OWT_data, ds[varname])
+                ds[varname] = xr.where(
+                    ~np.isnan(OWT_data), OWT_data, ds[varname]
+                )
             else:
                 ds[varname] = OWT_data
     return ds
